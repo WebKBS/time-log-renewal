3abbd8928e62b6fda78edc47c22e2703e0f16d02
gangbeomsu <kbs21c1987@gmail.com>
2024-08-11 09:59:48 +0900

    패키지 설치

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1>Time Log</h1>
       <p>출퇴근 시간을 기록하고 관리하는 웹 애플리케이션</p>
-      <Image src="/logo.svg" alt="logo" width={100} height={100} />
+      <Image src="/logo.svg" alt="logo" width={100} height={100}/>
     </div>
   );
 }
