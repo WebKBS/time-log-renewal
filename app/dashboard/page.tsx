16112556f2eb653ecacc52afa81fe4d38464c551
gangbeomsu <kbs21c1987@gmail.com>
2024-08-11 17:47:20 +0900

    색상 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NextTopLoader from "nextjs-toploader";
 import LogOut from "@/components/buttons/LogOut";
+import { Input } from "@/components/ui/input";
 
 const Page = () => {
   return (
     <div>
       <NextTopLoader showSpinner={false} color={"#6d28d9"} />
       <LogOut />
+      <Input type="datetime-local" />
     </div>
   );
 };
