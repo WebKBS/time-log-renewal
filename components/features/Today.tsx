0fe70739b0949e696162f4071d33dbf159552387
gangbeomsu <kbs21c1987@gmail.com>
2024-08-11 19:42:05 +0900

    오늘 시간 추가

---
+++ components/features/Today.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { UTCDate } from "@date-fns/utc";
+
+const Today = () => {
+  const today = new UTCDate();
+
+  const dayOfWeek = today.getDay();
+
+  return (
+    <div className="flex items-center text-sm">
+      <p>{today.toLocaleDateString("ko-KR", { timeZone: "Asia/Seoul" })}</p>
+      <p>
+        {dayOfWeek === 0
+          ? "일요일"
+          : dayOfWeek === 1
+            ? "월요일"
+            : dayOfWeek === 2
+              ? "화요일"
+              : dayOfWeek === 3
+                ? "수요일"
+                : dayOfWeek === 4
+                  ? "목요일"
+                  : dayOfWeek === 5
+                    ? "금요일"
+                    : "토요일"}
+      </p>
+    </div>
+  );
+};
+
+export default Today;
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeToggleButton } from "@/components/shadcn/ThemeToogleButton";
 import LogOut from "@/components/buttons/LogOut";
 import dynamic from "next/dynamic";
+import Today from "@/components/features/Today";
 
 const CurrentTime = dynamic(() => import("@/components/features/CurrentTime"), {
   ssr: false,
@@ -10,8 +11,8 @@ const CurrentTime = dynamic(() => import("@/components/features/CurrentTime"), {
 const Header = () => {
   return (
     <header className="">
-      <div className="flex items-center p-3 gap-4 justify-between">
-        <div></div>
+      <div className="flex items-center px-4 py-3 md:px-8 gap-4 justify-between">
+        <Today />
         <CurrentTime />
         <div className=" flex gap-2 items-center">
           <LogOut />
