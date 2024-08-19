b3970adf92c3b509ca7fd4522da7728e10a6eedc
gangbeomsu <kbs21c1987@gmail.com>
2024-08-19 20:56:14 +0900

    dvh 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,7 +16,7 @@ const LoginPage = async () => {
   }
 
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="relative h-dvh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted  text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-extrabold gap-2 p-10">
